495f264d5accf6b26d689388b1024195ac8081f1
Yanko <yanmark@gmail.com>
2017-10-17 04:20:08 +0300

    Add change Exam 19.02.2017-2

--- Exams/Java Fundamentals 19 February 2017 Part II/src/Ascent.java
+++ Exams/Java Fundamentals 19 February 2017 Part II/src/Ascent.java
@@ -1,95 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Ascent {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        String inputs = null;
-        Pattern pattern = Pattern.compile("((?<symbol>[,_])(?<message>[a-zA-Z]+|[,_][a-zA-Z]+[\\d])(?<digit>\\d))");
-
-        while (!(inputs = read.readLine()).equals("Ascend")) {
-            String[] input = inputs.split("\\s+");
-            String temporary = "";
-            String result = "";
-            StringBuilder res = new StringBuilder();
-            for (int i = 0; i < input.length; i++) {
-                Matcher matcher = pattern.matcher(input[i]);
-                String symbol = "";
-                String message = "";
-                int digit = 0;
-                int index = 0;
-
-                while (matcher.find()) {
-                    if (matcher.group("symbol") != null) {
-                        symbol = matcher.group("symbol");
-                    }
-                    if (matcher.group("message") != null) {
-                        message = matcher.group("message");
-                    }
-                    if (matcher.group("digit") != null) {
-                        digit = Integer.valueOf(matcher.group("digit"));
-                    }
-                    index = input[i].indexOf(matcher.group("digit")) + 1;
-                }
-                if (!message.equals("")) {
-                    if (message.contains(",") || message.contains("_")) {
-                        temporary = messagePattern(message);
-                        result = messages(symbol, temporary, digit);
-                        index++;
-                    } else {
-                        result = messages(symbol, message, digit);
-                    }
-                    res = res.append(result);
-                    if (index <= input[i].length() - 1) {
-                        String subStr = input[i].substring(index, input[i].length());
-                        res = res.append(subStr);
-                    }
-                    res = res.append(" ");
-                } else {
-                    res = res.append(input[i]).append(" ");
-                }
-            }
-            System.out.println(res);
-        }
-    }
-
-    private static String messages(String symbol, String message, int digit) {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < message.length(); i++) {
-            char temp = message.charAt(i);
-            if (symbol.equals(",")) {
-                temp += digit;
-            } else if (symbol.equals("_")) {
-                temp -= digit;
-            }
-            res = res.append(temp);
-        }
-        return res.toString();
-    }
-
-    private static String messagePattern(String str) {
-        Pattern pattern = Pattern.compile("((?<symbol>[,_])(?<message>[a-zA-Z]+)(?<digit>\\d))");
-        Matcher matcher = pattern.matcher(str);
-        String symbol = "";
-        String message = "";
-        int digit = 0;
-
-        while (matcher.find()) {
-            if (matcher.group("symbol") != null) {
-                symbol = matcher.group("symbol");
-            }
-            if (matcher.group("message") != null) {
-                message = matcher.group("message");
-            }
-            if (matcher.group("digit") != null) {
-                digit = Integer.valueOf(matcher.group("digit"));
-            }
-        }
-        return messages(symbol, message, digit);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		Pattern pattern = Pattern.compile("(,|_)([a-zA-Z]+)(\\d)");
+		List<String> text = new ArrayList<>();
+		Map<String, String> memos = new LinkedHashMap<>();
+		
+		String inputs;
+		
+		while (!(inputs = read.readLine()).equals("Ascend")) {
+			text.add(inputs);
+		}
+		
+		for (int i = 0; i < text.size(); i++) {
+			for (String m : memos.keySet()) {
+				text.set(i, text.get(i).replaceAll(m, memos.get(m)));
+			}
+			Matcher matcher = pattern.matcher(text.get(i));
+			while (matcher.find()) {
+				StringBuilder replacement = new StringBuilder("");
+				if (matcher.group(1).equals(",")) {
+					for (Character ch : matcher.group(2).toCharArray()) {
+						replacement.append((char) ((int) ch + Integer.parseInt(matcher.group(3))));
+					}
+				} else {
+					for (Character ch : matcher.group(2).toCharArray()) {
+						replacement.append((char) ((int) ch - Integer.parseInt(matcher.group(3))));
+					}
+				}
+				text.set(i, text.get(i).replace(matcher.group(), replacement.toString()));
+				memos.put(matcher.group(), replacement.toString());
+			}
+			System.out.println(text.get(i));
+		}
+	}
 }
-
